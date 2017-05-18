02a14c7ec9152fd5d5a048c369b44f6503002089
Anton <anton-loftschool@gunov.name>
2017-05-18 17:53:57 +0700

    added blur

--- gulp/paths/app.js
+++ gulp/paths/app.js
@@ -2,5 +2,6 @@
 
 module.exports = [
   './source/js/app.js',
-  './source/blocks/login-button/login-button.js'
+  './source/blocks/login-button/login-button.js',
+  './source/blocks/blur/blur.js'
 ];
