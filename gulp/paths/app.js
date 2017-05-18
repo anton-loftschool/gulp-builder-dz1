e6c66ddf34fee375bc73adc4da0872a9d228bdb8
Anton <anton-loftschool@gunov.name>
2017-05-18 21:25:02 +0700

    added hamburger

--- gulp/paths/app.js
+++ gulp/paths/app.js
@@ -3,5 +3,7 @@
 module.exports = [
   './source/js/app.js',
   './source/blocks/login-button/login-button.js',
-  './source/blocks/blur/blur.js'
+  // todo fix it
+  // './source/blocks/blur/blur.js',
+  './source/blocks/hamburger/hamburger.js'
 ];
---
+++ source/blocks/hamburger/hamburger.js
@@ -0,0 +1,4 @@
+$('.hamburger').click(function() {
+    $(this).toggleClass('active');
+    $('.overlay').toggleClass('open');
+});
