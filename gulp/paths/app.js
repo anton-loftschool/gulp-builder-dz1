27225675b240506e39c7197e6c521aeb4c77a295
Anton <anton-loftschool@gunov.name>
2017-05-21 17:51:34 +0700

    fixed preloader

--- gulp/paths/app.js
+++ gulp/paths/app.js
@@ -6,9 +6,9 @@ module.exports = [
   // todo fix it
   // './source/blocks/blur/blur.js',
   './source/blocks/hamburger/hamburger.js',
-  './source/blocks/spinner/spinner.js',
   './source/blocks/sidebar/sidebar-hide.js',
   './source/blocks/sidebar/sidebar-marker.js',
   './source/blocks/validation/validation.js',
-  './source/blocks/slider/slider.js'
+  './source/blocks/slider/slider.js',
+  './source/blocks/preloader/preloader.js'
 ];
